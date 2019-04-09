2fc44ea83e97b909b572b5c8e5a93313230d39b8
LAZIZ746 <LAZIZ757@MAIL.RU>
2019-04-09 15:03:44 +0300

    NTC

---
+++ .idea/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String text = "\n" +
+                "\n" +
+                "Люблю тебя, Петра творенье, Люблю твой строгий, стройный вид,Невы державное теченье, Береговой ее гранит, Твоих оград узор чугунный, Твоих задумчивых ночей Прозрачный сумрак, блеск безлунный, Когда я в комнате моей" +
+                "Пишу, читаю без лампады, И ясны спящие громады Пустынных улиц, и светла Адмиралтейская игла";
+
+
+
+        String[] words = text.toLowerCase().split(" ");
+        System.out.println(Arrays.toString(words));
+
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        Set<Map.Entry<String, Integer>> set = hm.entrySet();
+
+        for (String word : words) {
+            int value = 0;
+            try {
+                value = hm.get(word);
+            } catch(NullPointerException e) { }
+            hm.put(word, value + 1);
+        }
+        hm.forEach((k, v) -> System.out.println( k + ": " + v));
+
+
+        System.out.println("Стихиотворение имеет" + hm.size() + " неповторяющихся слов.");
+
+
+
+        PhoneBook pb = new PhoneBook();
+        pb.go();
+
+    }
+}
---
+++ .idea/src/com/company/PhoneBook.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.*;
+
+
+
+public class PhoneBook {private HashMap<String, PhoneRecord> pb;
+    private Scanner sc = new Scanner(System.in);
+    public PhoneBook() {
+        pb = new HashMap<String, PhoneRecord>();
+        pb.put("Ермолаев", new PhoneRecord("746-52-34", "ermolaev@mail.com"));
+        pb.put("Васюткин", new PhoneRecord("895-78-18", "vas@mail.com"));
+        pb.put("Беглов", new PhoneRecord("422-46-13", "beglov@mail.com"));
+        pb.put("Капрош", new PhoneRecord("746-11-41", "kaprosh@mail.com"));
+    }
+
+    public void go() {
+        System.out.print("Узнать номер телефона по фамилии: ");
+        System.out.println(getPhoneByName(sc.next().toLowerCase()));
+
+        System.out.print("Узнать электронную почту по фамилии:  ");
+        System.out.println(getEmailByName(sc.next().toLowerCase()));
+    }
+
+    public String getPhoneByName(String name) {
+        try {
+            PhoneRecord o = pb.get(name);
+            return o.getPhone();
+        } catch(NullPointerException e) {
+            return "Не найти";
+        }
+    }
+
+    public String getEmailByName(String name) {
+        try {
+            PhoneRecord o = pb.get(name);
+            return o.getEmail();
+        } catch(NullPointerException e) {
+            return "Не найти";
+        }
+    }
+}
